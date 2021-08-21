1671c2a0f3d4acdfed2a3c650fc16a485755edda
sojibdebnath <sojibdebnath.cse@gmail.com>
2021-08-21 21:11:25 +0600

    added

--- js/main.js
+++ js/main.js
@@ -1,70 +1,60 @@
-//iMAC price
-const bestPrice = document.getElementById('bestPrice').innerText;
-//memory call
-const freeMemory = document.getElementById('freeMemory');
-const extraMemory = document.getElementById('extraMemory');
-
-const memoryCost = document.getElementById('memoryCost');
-
-//storage call
-const normalSsd = document.getElementById('normalSsd');
-const middleSsd = document.getElementById('middleSsd');
-const highestSsd = document.getElementById('highestSsd');
-
-const storageCost = document.getElementById('storageCost');
-
-//deliveryCost
-const free = document.getElementById('free');
-const express = document.getElementById('express');
-
-const deliveryCost = document.getElementById('deliveryCost');
+//memory all
+function memory(num) {
+    memoryCost.innerText = num;
+}
+//storage all
+function storage(num) {
+    storageCost.innerText = num;
+}
+// delivery charge
+function delivery(num) {
+    deliveryCost.innerText = num;
+}
 
 //total price
 const partsCost = document.getElementById('totalPrice');
 
-
-
-//memoey update
-freeMemory.addEventListener('click', function () {
-    memoryCost.innerText = '0';
+//memory update
+document.getElementById('freeMemory').addEventListener('click', function () {
+    memory('0');
     updateTotal();
 });
-extraMemory.addEventListener('click', function () {
-    memoryCost.innerText = '180';
+document.getElementById('extraMemory').addEventListener('click', function () {
+    memory('180');
     updateTotal();
 });
 
 //storage update
-normalSsd.addEventListener('click', function () {
-    storageCost.innerText = '0';
+document.getElementById('normalSsd').addEventListener('click', function () {
+    storage('0');
     updateTotal();
 });
-middleSsd.addEventListener('click', function () {
-    storageCost.innerText = '100';
+document.getElementById('middleSsd').addEventListener('click', function () {
+    storage('100');
     updateTotal();
 });
-highestSsd.addEventListener('click', function () {
-    storageCost.innerText = '180';
+document.getElementById('highestSsd').addEventListener('click', function () {
+    storage('180');
     updateTotal();
 });
 
 //Delivery update
-free.addEventListener('click', function () {
-    deliveryCost.innerText = '0';
+document.getElementById('free').addEventListener('click', function () {
+    delivery('0');
     updateTotal();
 });
-express.addEventListener('click', function () {
-    deliveryCost.innerText = '20';
+document.getElementById('express').addEventListener('click', function () {
+    delivery('20');
     updateTotal();
 });
 
 // update Total
 function updateTotal() {
-    const mainPrice = Number('1299');
+    const iMacPrice = Number('1299');
     const memoryPrice = Number(memoryCost.innerText);
     const storagePrice = Number(storageCost.innerText);
     const expressCost = Number(deliveryCost.innerText);
-    const totalPriceUpdate = mainPrice + memoryPrice + storagePrice + expressCost;
+    const totalPriceUpdate = iMacPrice + memoryPrice + storagePrice + expressCost;
     totalPrice.innerText = totalPriceUpdate;
     document.getElementById('updateTotal').innerText = totalPriceUpdate;
 
@@ -72,7 +62,7 @@ function updateTotal() {
     document.getElementById('pomoButton').addEventListener('click', function () {
         const pomoField = document.getElementById('pomocode');
         if (pomoField.value == 'stevenkaku') {
-            const discount = parseFloat((20 / 100) * totalPriceUpdate);
+            const discount = ((20 / 100) * totalPriceUpdate);
             const newDiscountBalance = partsCost.innerText - discount;
 
             document.getElementById('updateTotal').innerText = newDiscountBalance;
@@ -82,3 +72,91 @@ function updateTotal() {
 
     });
 };
+
+
+
+
+
+
+
+
+// function memory(num) {
+//     memoryCost.innerText = num;
+// }
+
+// //storage call
+// const normalSsd = document.getElementById('normalSsd');
+// const middleSsd = document.getElementById('middleSsd');
+// const highestSsd = document.getElementById('highestSsd');
+
+// const storageCost = document.getElementById('storageCost');
+
+// //deliveryCost
+// const free = document.getElementById('free');
+// const express = document.getElementById('express');
+
+// const deliveryCost = document.getElementById('deliveryCost');
+
+// //total price
+// const partsCost = document.getElementById('totalPrice');
+
+
+
+// //memoey update
+// document.getElementById('freeMemory').addEventListener('click', function () {
+//     memory('0');
+//     updateTotal();
+// });
+// document.getElementById('extraMemory').addEventListener('click', function () {
+//     memory('180');
+//     updateTotal();
+// });
+
+// //storage update
+// normalSsd.addEventListener('click', function () {
+//     storageCost.innerText = '0';
+//     updateTotal();
+// });
+// middleSsd.addEventListener('click', function () {
+//     storageCost.innerText = '100';
+//     updateTotal();
+// });
+// highestSsd.addEventListener('click', function () {
+//     storageCost.innerText = '180';
+//     updateTotal();
+// });
+
+// //Delivery update
+// free.addEventListener('click', function () {
+//     deliveryCost.innerText = '0';
+//     updateTotal();
+// });
+// express.addEventListener('click', function () {
+//     deliveryCost.innerText = '20';
+//     updateTotal();
+// });
+
+// // update Total
+// function updateTotal() {
+//     const mainPrice = Number('1299');
+//     const memoryPrice = Number(memoryCost.innerText);
+//     const storagePrice = Number(storageCost.innerText);
+//     const expressCost = Number(deliveryCost.innerText);
+//     const totalPriceUpdate = mainPrice + memoryPrice + storagePrice + expressCost;
+//     totalPrice.innerText = totalPriceUpdate;
+//     document.getElementById('updateTotal').innerText = totalPriceUpdate;
+
+//     //discount added
+//     document.getElementById('pomoButton').addEventListener('click', function () {
+//         const pomoField = document.getElementById('pomocode');
+//         if (pomoField.value == 'stevenkaku') {
+//             const discount = parseFloat((20 / 100) * totalPriceUpdate);
+//             const newDiscountBalance = partsCost.innerText - discount;
+
+//             document.getElementById('updateTotal').innerText = newDiscountBalance;
+//         }
+
+//         pomoField.value = '';
+
+//     });
+// };
